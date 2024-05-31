0a691e618df5a4534673da86a39518a46a866537
PanjvaniGaurav <panjvanigaurav4@gmail.com>
2024-05-31 16:32:59 +0530

    created Login Page Structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { LogoURL } from '../utils/constants'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='absolute px-10 py-3 bg-gradient-to-b from-black w-full z-10'>
+        <img className='w-44' src={LogoURL} alt='Logo'/>
+    </div>
   )
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Header from './Header'
+import { BackgroundURL } from '../utils/constants'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+return (
+    <div>
+        <Header />
+        <div className='absolute'>
+            <img src={BackgroundURL} alt="Background Image" />
+        </div>
+        <form className='flex absolute flex-col w-3/12 my-48 mx-auto left-0 right-0 p-12 text-white bg-black rounded-md bg-opacity-80'>
+            <h1 className='m-2 font-bold text-3xl py-4'>Sign In</h1>
+            <input type='text' placeholder='Email Address' className='p-3 my-3 w-full bg-neutral-700    '/>
+            <input type='password' placeholder='Password' className='p-3 my-3 w-full bg-neutral-700'/>
+            <button className='bg-red-600 my-4 p-2 w-full text-lg rounded-md'>Sign In</button>
+        </form>
+    </div>
+)
 }
 
 export default Login
