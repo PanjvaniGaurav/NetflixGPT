0616bbe73cc99de5b12ba2200d79ac415c7ead6e
PanjvaniGaurav <panjvanigaurav4@gmail.com>
2024-05-31 15:25:59 +0530

    created Routing

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-import Header from './Header'
 import Login from './Login'
 import Browse from './Browse'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
 const Body = () => {
+    const appRouter = createBrowserRouter([
+        {
+            path:"/",
+            element:<Login />
+        },
+        {
+            path:"/browse",
+            element:<Browse/>
+        }
+    ])
+
   return (
     <div>
-        <Header />
-        <Login />
-        <Browse />
+        <RouterProvider router={appRouter} />
     </div>
   )
 }
