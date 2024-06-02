49e753ceff9cfa9e309267dc525150c82d6586bb
PanjvaniGaurav <panjvanigaurav4@gmail.com>
2024-06-02 15:13:46 +0530

    Displayed user name in browse page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,11 +23,13 @@ const Header = () => {
     <div className='absolute px-10 py-3 bg-gradient-to-b from-black w-full z-10 flex justify-between'>
         <img className='w-44' src={LogoURL} alt='Logo'/>
         
-          {/* user && */}
-          <div className='flex justify-center items-center'>
+          {user &&
+          (<div className='flex justify-center items-center'>
+          <h1 className='text-red-500 text-2xl mr-5'>Welcome {user.displayName !== null ? user.displayName : 'User'}</h1> 
           <img className='h-20' src={usericon} alt='User Icon'/>
           <button onClick={handleSignout} className='bg-red-600 text-white px-3 py-1 rounded-lg h-14'>Sign Out</button>
-        </div>
+           </div>
+          )}
         
     </div>
   )
